0432043639fcdde69af807656ff6b0a77506d02d
Javier Alejandro <joalmar24@gmail.com>
2026-02-26 05:34:03 +0000

    Implementada baja logica socios y ajustes en listado

--- apps/socios/urls.py
+++ apps/socios/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('crear/', views.crear_socio, name='crear_socio'),
+    path('editar/<int:pk>/', views.editar_socio, name='editar_socio'),
+    path('eliminar/<int:pk>/', views.eliminar_socio, name='eliminar_socio'),
 ]
--- apps/socios/views.py
+++ apps/socios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .models import Socio
 from .forms import SocioForm
 
@@ -16,3 +17,19 @@ def crear_socio(request):
         form = SocioForm()
 
     return render(request, 'socios/crear.html', {'form': form})
+def editar_socio(request, pk):
+    socio = get_object_or_404(Socio, pk=pk)
+
+    if request.method == 'POST':
+        form = SocioForm(request.POST, instance=socio)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = SocioForm(instance=socio)
+
+    return render(request, 'socios/crear.html', {'form': form})
+def eliminar_socio(request, pk):
+    socio = get_object_or_404(Socio, pk=pk)
+    socio.delete()
+    return redirect('home')
